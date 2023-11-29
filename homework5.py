53d0d643f95c6abddd4253ceed4ecb9362b74afb
Yuriy Nyu <sonicmemez@gmail.com>
2023-11-29 11:48:21 +0300

    task 1

--- homework5.py
+++ homework5.py
@@ -1,5 +1,38 @@
-cook_book = {}
-
+from pprint import pprint
 with open("recipes.txt", encoding='utf-8') as recipes:
-    print(recipes.read())
+    # print(recipes.read())
+    lines = recipes.read().splitlines()
+
+def split_list(input_list, separator):
+    sublist = []
+    for item in input_list:
+        if item == separator:
+            yield sublist
+            sublist = []
+        else:
+            sublist.append(item)
+    yield sublist
     
+def transform_data(data):
+    dish_name = data[0]
+    ingredients = data[2:]
+    transformed_ingredients = []
+
+    for ingredient in ingredients:
+        ingredient_name, quantity, measure = ingredient.split(' | ')
+        transformed_ingredients.append({
+            'ingredient_name': ingredient_name,
+            'quantity': int(quantity),
+            'measure': measure
+        })
+
+    return {dish_name: transformed_ingredients}
+
+def task_1():
+    cook_book = {}
+    cook_list = list(split_list(lines, ''))
+    for dish in cook_list:  
+        cook_book.update(transform_data(dish))
+    return cook_book
+
+pprint(task_1(), width=1, sort_dicts=False)
