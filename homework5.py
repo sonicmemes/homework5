422f61ffdb2d826e15d6b46aea32d38393f32563
Yuriy Nyu <sonicmemez@gmail.com>
2023-12-01 18:40:57 +0300

    end work

--- homework5.py
+++ homework5.py
@@ -2,13 +2,13 @@ from pprint import pprint
 from collections import Counter
 import os
 
-with open("recipes.txt", encoding='utf-8') as recipes:
-    lines = recipes.read().splitlines()
-
-
+path_orig = "recipes.txt"
 path1 = "1.txt"
 path2 = "2.txt"
 path3 = "3.txt"
+
+with open(path_orig, encoding='utf-8') as recipes:
+    lines = recipes.read().splitlines()
 
 with open(path1, encoding='utf-8') as file_1:
     file1 = file_1.read().splitlines()
@@ -77,58 +77,46 @@ def get_shop_list_by_dishes(dishes, person_count):
     dict_result = dict(sort_result)
     return dict_result
 
-
 def task_3(item1, item2, item3):
     f1count = 0
     f2count = 0
     f3count = 0
-
+    dict1 = {}
+    dict2 = {}
+    dict3 = {}
+    main_list = []
     for row in item1:
-        # print(row)
         f1count = f1count + 1 
     for row in item2:
-        # print(row)
         f2count = f2count + 1 
     for row in item3:
-        # print(row)
         f3count = f3count + 1 
-    # print(f1count)
-    # print(f2count)
-    # print(f3count)
     filename1 = os.path.basename(path1)
     filename2 = os.path.basename(path2)
     filename3 = os.path.basename(path3)
-    # print(filename1)
-    # print(filename2)
-    # print(filename3)
-    filenames = [filename1, filename2, filename3]
-    lines_count = [f1count, f2count, f3count]
-    lines = [item1, item2, item3]
-    dict1 = {'dict1': {'filename': filename1,'line_count': f1count, 'line': item1}}
-    dict2 = {'dict2': {'filename': filename2,'line_count': f2count, 'line': item2}}
-    dict3 = {'dict3': {'filename': filename3,'line_count': f3count, 'line': item3}}
-    main_dict = {}
-    list_num = []
-    main_dict.update(dict1)
-    main_dict.update(dict2)
-    main_dict.update(dict3)
-    # print(main_dict)
-    for keys in main_dict:
-        dicts = main_dict[keys]
-        list_num.append(dicts['line_count'])
-        print(dicts_srt)
+    dict1 = {'filename': filename1, 'line_count': f1count, 'line': item1}
+    dict2 = {'filename': filename2, 'line_count': f2count, 'line': item2}
+    dict3 = {'filename': filename3, 'line_count': f3count, 'line': item3}
+    main_list.append(dict1)
+    main_list.append(dict2)
+    main_list.append(dict3)
+    sorted_list = sorted(main_list, key=lambda x: x['line_count'])
+    line1 = '\n'.join(sorted_list[0]['line'])
+    line2 = '\n'.join(sorted_list[1]['line'])
+    line3 = '\n'.join(sorted_list[2]['line'])
+    result = (f"{sorted_list[0]['filename']} \n"
+    f"{sorted_list[0]['line_count']} \n"
+    f"{line1} \n"
+    f"{sorted_list[1]['filename']} \n"
+    f"{sorted_list[1]['line_count']} \n"
+    f"{line2} \n"
+    f"{sorted_list[2]['filename']} \n"
+    f"{sorted_list[2]['line_count']} \n"
+    f"{line3} \n")
+    return result   
+   
+pprint(task_1(), indent=4, sort_dicts=False)
+pprint(get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2), indent=4, sort_dicts=False)
 
-        # print(dicts)
-        
-    # main_dict_sorted = 0
-    # print(main_dict_sorted)
-
-    with open("result.txt", 'w', encoding='utf-8') as result:
-        # result.write(str(new_list))
-        pass
-        
-            
-
-task_3(file1, file2, file3)
-# pprint(task_1(), indent=4, sort_dicts=False)
-# pprint(get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2), indent=4, sort_dicts=False)
+with open("result.txt", 'w', encoding='utf-8') as result:
+        result.write(str(task_3(file1, file2, file3)))
